89cd17b9d9ca07402e81673181323e6240da1b16
mohongshu <mo.hongshu@163.com>
2016-08-22 18:21:39 +0800

    333

--- mo.sh
+++ mo.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 #懒惰脚本
-hostnames=(192.168.209.128 192.168.209.141 192.168.209.142)
+#hostnames=(192.168.209.128 192.168.209.141 192.168.209.142)
+servers=$1
+hostfile=/root/mo.sh/hostnames
+grep "^\s*#" 
+sed -n "/$servers/,/$servers/p"
+for s in servers;do
+  
+done
 for h in ${hostnames[*]};do
-  echo $h
-  ssh -t $h $1
+  echo "Host $h Start--------------"
+  echo ""
+
+  ssh -t $h $2
+
+  echo ""
+  echo "Host $h End----------------"
 done
